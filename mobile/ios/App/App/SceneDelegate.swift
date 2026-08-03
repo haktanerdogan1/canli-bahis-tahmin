9354260382bd916c2edbff46e0f5bb7ac37e3008
Haktan Erdoğan <seninmail@ornek.com>
2026-08-04 02:39:35 +0300

    Uygulama her acilista WKWebView onbellegini temizlesin

--- mobile/ios/App/App/SceneDelegate.swift
+++ mobile/ios/App/App/SceneDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import WebKit
 import Capacitor
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -7,6 +8,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
 
+        // App yalnizca uzaktaki (Railway) siteyi gosteren bir kabuk - kalici bir
+        // WKWebView onbellegi burada hicbir fayda saglamiyor, sadece her sunucu
+        // guncellemesinden sonra eski sayfanin gorunmesine yol aciyordu. Her
+        // acilista temizleyip her zaman en guncel siteyi cekmesini garantiliyoruz.
+        WKWebsiteDataStore.default().removeData(
+            ofTypes: WKWebsiteDataStore.allWebsiteDataTypes(),
+            modifiedSince: .distantPast
+        ) { [weak self] in
+            self?.setUpWindow(in: windowScene)
+        }
+
+        SceneDelegateProxy.shared.scene(scene, willConnectTo: session, options: connectionOptions)
+    }
+
+    private func setUpWindow(in windowScene: UIWindowScene) {
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = CAPBridgeViewController()
         window?.makeKeyAndVisible()
@@ -18,8 +34,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             splash.layoutIfNeeded()
             splash.playIntro { }
         }
-
-        SceneDelegateProxy.shared.scene(scene, willConnectTo: session, options: connectionOptions)
     }
 
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
